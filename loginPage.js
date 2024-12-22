54354bfb7070ed26a0f81091be7a87c8d86bc9c0
onkar-satpute <omsatpute0109@gmail.com>
2024-12-22 14:36:09 +0530

    registration from create

---
+++ loginPage.js
@@ -0,0 +1,20 @@
+function validateForm() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMsg = document.getElementById('error-msg');
+
+    if (username.length < 3) {
+        errorMsg.innerHTML = 'Username must be at least 3 characters long.';
+        return false;
+    }
+
+    if (password.length < 6) {
+        errorMsg.innerHTML = 'Password must be at least 6 characters long.';
+        return false;
+    }
+
+    errorMsg.innerHTML = '';
+    alert('Login successful!');
+    window.location.assign("main.html");
+    return false;
+}
